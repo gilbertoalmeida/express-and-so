b1c7861a01b1e5bb1428deb19eb203aa3061d73c
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-11-19 00:08:48 +0100

    Using the Navigator api to get users position, ploting it on a map of leaflet. I am also creating the server in another way to be able to add a self-signed certificate.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //import express from "express"
 const express = require("express")
+const fs = require('fs')
+const https = require('https')
 
 const app = express()
 
@@ -11,8 +13,24 @@ const app = express()
 //   })
 // })
 
+// app.get("/", (req, res) => {
+//   res.send("oi")
+// })
+
 app.use(express.static("public"))
 
-app.listen(3000, () => {
+var privateKey = fs.readFileSync('./localhost.key');
+var certificate = fs.readFileSync('./localhost.crt');
+
+//I am creating the server this way, bc I needed https for the map to work on my phone. I created a self-signed certificate for localhost and trusted it on my mac.
+//Files are not in the repository, you have to create them, or create the server the normal away below and use http
+https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app).listen(3000, () => {
   console.log("App listening port 3000")
-})
+});
+
+// app.listen(3000, () => {
+//   console.log("App listening port 3000")
+// })
